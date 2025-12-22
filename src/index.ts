40951b92538e6460e02bfcfa07b43167ecc54c2d
Duong Rakan <duong.cv.hec@gmail.com>
2025-12-23 01:37:17 +0700

    update cors v2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import databaseService from '~/services/database.services'
 import usersRouter from './routes/users.routes'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import moviesRouter from './routes/movies.routes'
 import analyticsRouter from './routes/analytics.routes'
 import ratingsRouter from './routes/ratings.routes'
@@ -13,18 +13,30 @@ const app = express()
 const port = 4000
 
 app.use(express.json())
-const allowedOrigins = ['http://localhost:3000', 'https://movie-website-a6g6j6l95-duong122702s-projects.vercel.app/']
-const corsOption = {
-  origin: allowedOrigins,
+const corsOption: CorsOptions = {
+  origin: (origin, callback) => {
+    // Cho phép Postman, Server-to-Server (không có origin)
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    // Cho phép Localhost HOẶC bất kỳ link nào có đuôi .vercel.app
+    if (origin.includes('localhost') || origin.endsWith('.vercel.app')) {
+      callback(null, true)
+    } else {
+      callback(new Error(`Not allowed by CORS: ${origin}`))
+    }
+  },
   optionsSuccessStatus: 200,
-  credentials: true
+  credentials: true // 3. FIX LỖI CHÍNH TẢ: Phải viết thường chữ 'c'
 }
-app.use('/users', cors(corsOption), usersRouter)
-app.use('/movies', cors(corsOption), moviesRouter)
-app.use('/analytics', cors(corsOption), analyticsRouter)
-app.use('/comments', cors(corsOption), commentsRouter)
-app.use('/ratings', cors(corsOption), ratingsRouter)
-app.use('/favorites', cors(corsOption), favoritesRouter)
+app.use(cors(corsOption))
+app.use('/users', usersRouter)
+app.use('/movies', moviesRouter)
+app.use('/analytics', analyticsRouter)
+app.use('/comments', commentsRouter)
+app.use('/ratings', ratingsRouter)
+app.use('/favorites', favoritesRouter)
 // Default Error handler
 app.use(defaultErrorHandler)
 app.listen(port, () => {
